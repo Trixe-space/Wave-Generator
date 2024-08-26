6bd493492aa3450a4f2eace386745b6d9fc94134
Trixe space <trixe-space@trixe-space.tk>
2024-08-26 21:08:16 +0600

    Managed to log inputs to the console.

--- code.js
+++ code.js
@@ -1,6 +1,31 @@
 const canvas = document.getElementById('output')
 const c = canvas.getContext('2d')
 
+const colorInput = document.getElementById('color')
+const amplitudeInput = document.getElementById('amplitude')
+const wavelengthInput = document.getElementById('wavelength')
+
+let color = colorInput.value
+let amplitude = amplitudeInput.value
+let wavelength = wavelengthInput.value
+
+
+colorInput.addEventListener('input', () => {
+    color = colorInput.value
+    drawWave()
+})
+
+amplitudeInput.addEventListener('input', () => {
+    amplitude = amplitudeInput.value
+    drawWave()
+})
+
+wavelengthInput.addEventListener('input', () => {
+    wavelength = wavelengthInput.value
+    drawWave()
+})
+
+
 const ratio = 100
 
 const drawGraph = () => {
@@ -9,6 +34,7 @@ const drawGraph = () => {
         c.moveTo(i * ratio, 0)
         c.lineTo(i * ratio, canvas.height)
         c.strokeStyle = '#202020'
+        c.lineWidth = 3
         c.stroke()
     }
 
@@ -17,17 +43,24 @@ const drawGraph = () => {
             c.beginPath()
             c.moveTo(0, i * ratio)
             c.lineTo(canvas.width, i * ratio)
-            c.lineWidth = 5
+            c.lineWidth = 10
             c.stroke()
         } else {
             c.beginPath()
             c.moveTo(0, i * ratio)
             c.lineTo(canvas.width, i * ratio)
             c.strokeStyle = '#202020'
-            c.lineWidth = 2
+            c.lineWidth = 3
             c.stroke()
         }
     }
 }
 
-drawGraph()
+const drawWave = () => {
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    drawGraph()
+
+    console.log(color, amplitude, wavelength)
+}
+
+drawWave()
